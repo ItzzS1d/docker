786755841ae81bffff084a395908d926bd897e49
itzzs1d <sanamanisiddharam123@gmail.com>
2025-01-16 23:05:13 +0530

    removed localhost:3000

--- dist/assets/Home-Th20gMDy.js
+++ dist/assets/Home-Th20gMDy.js
@@ -1,1 +1,193 @@
-import{r as i,u as m,b as u,j as e}from"./index-BZuEKrGi.js";const d="_home_4nqpm_4",h="_formContainer_4nqpm_13",f="_heading_4nqpm_22",p="_formGroup_4nqpm_28",x="_label_4nqpm_32",_="_formSelect_4nqpm_39",j="_formInput_4nqpm_40",g="_formButton_4nqpm_55",v="_logoutBtn_4nqpm_70",o={home:d,formContainer:h,heading:f,formGroup:p,label:x,formSelect:_,formInput:j,formButton:g,logoutBtn:v};function N(n){let t=null;switch(n){case"form6":t=6;break;case"form8":t=7;break;case"nmrs":t=8;break;case"wagelist":t=11;break;case"materialist":t=13;break;default:t=null;break}return t}const C=()=>{const[n,t]=i.useState({workCode:"",srNo:"",finance_year:""}),{setIsAuth:s}=m(),a=u(),l=async r=>{r.preventDefault(),a(`/document?Srno=${N(n.srNo)}&financial_year=${n.finance_year}&workCode=${n.workCode}`)},c=async()=>{try{const r=await fetch("http://localhost:3000/api/v1/auth/logout",{method:"DELETE",credentials:"include"});r.ok?(s(!1),a("/login")):(console.error("Failed to logout. Status:",r.status),alert("Logout failed. Please try again."))}catch(r){console.error("An error occurred during logout:",r)}};return e.jsxs(e.Fragment,{children:[e.jsx("button",{onClick:c,className:o.logoutBtn,children:"Logout"}),e.jsx("div",{className:o.home,children:e.jsxs("form",{onSubmit:l,className:o.formContainer,children:[e.jsx("h1",{className:o.heading,children:"Document Request Form"}),e.jsxs("div",{className:o.formGroup,children:[e.jsx("label",{htmlFor:"srNo",className:o.label,children:"SR No."}),e.jsxs("select",{name:"srNo",className:o.formSelect,onChange:r=>t({...n,srNo:r.target.value.toLowerCase()}),required:!0,children:[e.jsx("option",{value:"form6",children:"Form 6"}),e.jsx("option",{value:"form8",children:"Form 8"}),e.jsx("option",{value:"nmrs",children:"NMRS"}),e.jsx("option",{value:"wagelist",children:"WAGELIST"}),e.jsx("option",{value:"materialist",children:"MATERIAL List"}),e.jsx("option",{value:"vender",children:"VENDER"}),e.jsx("option",{value:"13",children:"DPR FORZEN"})]})]}),e.jsxs("div",{className:o.formGroup,children:[e.jsx("label",{htmlFor:"finance_year",className:o.label,children:"Financial Year"}),e.jsxs("select",{name:"finance_year",className:o.formSelect,onChange:r=>t({...n,finance_year:r.target.value}),required:!0,children:[e.jsx("option",{value:"",disabled:!0,selected:!0,children:"Select Financial Year"}),e.jsx("option",{value:"2020-2021",children:"2020-2021"}),e.jsx("option",{value:"2021-2022",children:"2021-2022"}),e.jsx("option",{value:"2022-2023",children:"2022-2023"}),e.jsx("option",{value:"2023-2024",children:"2023-2024"}),e.jsx("option",{value:"2024-2025",children:"2024-2025"})]})]}),e.jsxs("div",{className:o.formGroup,children:[e.jsx("label",{htmlFor:"workCode",className:o.label,children:"Work Code"}),e.jsx("input",{type:"text",className:o.formInput,placeholder:"Enter Work Code",value:n.workCode,onChange:r=>t({...n,workCode:r.target.value}),required:!0})]}),e.jsx("button",{className:o.formButton,children:"Get Document"})]})})]})};export{C as default};
+import { r as i, u as m, b as u, j as e } from "./index-BZuEKrGi.js";
+const d = "_home_4nqpm_4",
+  h = "_formContainer_4nqpm_13",
+  f = "_heading_4nqpm_22",
+  p = "_formGroup_4nqpm_28",
+  x = "_label_4nqpm_32",
+  _ = "_formSelect_4nqpm_39",
+  j = "_formInput_4nqpm_40",
+  g = "_formButton_4nqpm_55",
+  v = "_logoutBtn_4nqpm_70",
+  o = {
+    home: d,
+    formContainer: h,
+    heading: f,
+    formGroup: p,
+    label: x,
+    formSelect: _,
+    formInput: j,
+    formButton: g,
+    logoutBtn: v,
+  };
+function N(n) {
+  let t = null;
+  switch (n) {
+    case "form6":
+      t = 6;
+      break;
+    case "form8":
+      t = 7;
+      break;
+    case "nmrs":
+      t = 8;
+      break;
+    case "wagelist":
+      t = 11;
+      break;
+    case "materialist":
+      t = 13;
+      break;
+    default:
+      t = null;
+      break;
+  }
+  return t;
+}
+const C = () => {
+  const [n, t] = i.useState({ workCode: "", srNo: "", finance_year: "" }),
+    { setIsAuth: s } = m(),
+    a = u(),
+    l = async (r) => {
+      r.preventDefault(),
+        a(
+          `/document?Srno=${N(n.srNo)}&financial_year=${
+            n.finance_year
+          }&workCode=${n.workCode}`
+        );
+    },
+    c = async () => {
+      try {
+        const r = await fetch("/api/v1/auth/logout", {
+          method: "DELETE",
+          credentials: "include",
+        });
+        r.ok
+          ? (s(!1), a("/login"))
+          : (console.error("Failed to logout. Status:", r.status),
+            alert("Logout failed. Please try again."));
+      } catch (r) {
+        console.error("An error occurred during logout:", r);
+      }
+    };
+  return e.jsxs(e.Fragment, {
+    children: [
+      e.jsx("button", {
+        onClick: c,
+        className: o.logoutBtn,
+        children: "Logout",
+      }),
+      e.jsx("div", {
+        className: o.home,
+        children: e.jsxs("form", {
+          onSubmit: l,
+          className: o.formContainer,
+          children: [
+            e.jsx("h1", {
+              className: o.heading,
+              children: "Document Request Form",
+            }),
+            e.jsxs("div", {
+              className: o.formGroup,
+              children: [
+                e.jsx("label", {
+                  htmlFor: "srNo",
+                  className: o.label,
+                  children: "SR No.",
+                }),
+                e.jsxs("select", {
+                  name: "srNo",
+                  className: o.formSelect,
+                  onChange: (r) =>
+                    t({ ...n, srNo: r.target.value.toLowerCase() }),
+                  required: !0,
+                  children: [
+                    e.jsx("option", { value: "form6", children: "Form 6" }),
+                    e.jsx("option", { value: "form8", children: "Form 8" }),
+                    e.jsx("option", { value: "nmrs", children: "NMRS" }),
+                    e.jsx("option", {
+                      value: "wagelist",
+                      children: "WAGELIST",
+                    }),
+                    e.jsx("option", {
+                      value: "materialist",
+                      children: "MATERIAL List",
+                    }),
+                    e.jsx("option", { value: "vender", children: "VENDER" }),
+                    e.jsx("option", { value: "13", children: "DPR FORZEN" }),
+                  ],
+                }),
+              ],
+            }),
+            e.jsxs("div", {
+              className: o.formGroup,
+              children: [
+                e.jsx("label", {
+                  htmlFor: "finance_year",
+                  className: o.label,
+                  children: "Financial Year",
+                }),
+                e.jsxs("select", {
+                  name: "finance_year",
+                  className: o.formSelect,
+                  onChange: (r) => t({ ...n, finance_year: r.target.value }),
+                  required: !0,
+                  children: [
+                    e.jsx("option", {
+                      value: "",
+                      disabled: !0,
+                      selected: !0,
+                      children: "Select Financial Year",
+                    }),
+                    e.jsx("option", {
+                      value: "2020-2021",
+                      children: "2020-2021",
+                    }),
+                    e.jsx("option", {
+                      value: "2021-2022",
+                      children: "2021-2022",
+                    }),
+                    e.jsx("option", {
+                      value: "2022-2023",
+                      children: "2022-2023",
+                    }),
+                    e.jsx("option", {
+                      value: "2023-2024",
+                      children: "2023-2024",
+                    }),
+                    e.jsx("option", {
+                      value: "2024-2025",
+                      children: "2024-2025",
+                    }),
+                  ],
+                }),
+              ],
+            }),
+            e.jsxs("div", {
+              className: o.formGroup,
+              children: [
+                e.jsx("label", {
+                  htmlFor: "workCode",
+                  className: o.label,
+                  children: "Work Code",
+                }),
+                e.jsx("input", {
+                  type: "text",
+                  className: o.formInput,
+                  placeholder: "Enter Work Code",
+                  value: n.workCode,
+                  onChange: (r) => t({ ...n, workCode: r.target.value }),
+                  required: !0,
+                }),
+              ],
+            }),
+            e.jsx("button", {
+              className: o.formButton,
+              children: "Get Document",
+            }),
+          ],
+        }),
+      }),
+    ],
+  });
+};
+export { C as default };
--- dist/assets/Login-Cr2XcJQ4.js
+++ dist/assets/Login-Cr2XcJQ4.js
@@ -1,1 +1,132 @@
-import{r as t,a as j,b as v,u as N,j as e}from"./index-BZuEKrGi.js";const w=()=>{const[s,n]=t.useState({username:"",password:""}),[p,o]=t.useState(!1),[i,h]=t.useState(""),l=j(),a=v(),{isSessionExpired:m,setIsSessionExpired:x,setIsAuth:c,isAuth:f}=N(),g=async r=>{r.preventDefault();try{o(!0);const d=await(await fetch("http://localhost:3000/api/v1/auth/login",{method:"POST",headers:{"Content-type":"application/json"},credentials:"include",body:JSON.stringify(s)})).json();if(d.error){h(d.error);return}else l.search.startsWith("?returnUrl")?(c(!0),a(l.search.replace("?returnUrl=",""))):(c(!0),a("/")),x(!1)}catch(u){console.error("Error while loggin please try again",u)}finally{o(!1)}};return t.useLayoutEffect(()=>{f&&a("/")},[]),e.jsxs(e.Fragment,{children:[m&&e.jsx("h1",{className:"sessionExpired",children:"session expired please login "}),e.jsxs("form",{className:"modern-form",onSubmit:g,children:[e.jsx("div",{className:"form-title",children:"Sign In"}),e.jsxs("div",{className:"form-body",children:[e.jsx("div",{className:"input-group",children:e.jsxs("div",{className:"input-wrapper",children:[e.jsx("svg",{fill:"none",viewBox:"0 0 24 24",className:"input-icon",children:e.jsx("path",{strokeWidth:"1.5",stroke:"currentColor",d:"M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z"})}),e.jsx("input",{required:!0,placeholder:"Username",className:"form-input",type:"text",value:s.username,onChange:r=>n({...s,username:r.target.value})})]})}),e.jsxs("div",{className:"input-group",children:[e.jsxs("div",{className:"input-wrapper",children:[e.jsx("svg",{fill:"none",viewBox:"0 0 24 24",className:"input-icon",children:e.jsx("path",{strokeWidth:"1.5",stroke:"currentColor",d:"M12 10V14M8 6H16C17.1046 6 18 6.89543 18 8V16C18 17.1046 17.1046 18 16 18H8C6.89543 18 6 17.1046 6 16V8C6 6.89543 6.89543 6 8 6Z"})}),e.jsx("input",{required:!0,placeholder:"Password",className:"form-input",type:"password",value:s.password,onChange:r=>n({...s,password:r.target.value})})]}),i&&e.jsx("p",{className:"error",children:i})]})]}),e.jsxs("button",{className:"submit-button",type:"submit",disabled:p,children:[e.jsx("span",{className:"button-text",children:"Login"}),e.jsx("div",{className:"button-glow"})]})]})]})};export{w as default};
+import { r as t, a as j, b as v, u as N, j as e } from "./index-BZuEKrGi.js";
+const w = () => {
+  const [s, n] = t.useState({ username: "", password: "" }),
+    [p, o] = t.useState(!1),
+    [i, h] = t.useState(""),
+    l = j(),
+    a = v(),
+    {
+      isSessionExpired: m,
+      setIsSessionExpired: x,
+      setIsAuth: c,
+      isAuth: f,
+    } = N(),
+    g = async (r) => {
+      r.preventDefault();
+      try {
+        o(!0);
+        const d = await (
+          await fetch("/api/v1/auth/login", {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify(s),
+          })
+        ).json();
+        if (d.error) {
+          h(d.error);
+          return;
+        } else
+          l.search.startsWith("?returnUrl")
+            ? (c(!0), a(l.search.replace("?returnUrl=", "")))
+            : (c(!0), a("/")),
+            x(!1);
+      } catch (u) {
+        console.error("Error while loggin please try again", u);
+      } finally {
+        o(!1);
+      }
+    };
+  return (
+    t.useLayoutEffect(() => {
+      f && a("/");
+    }, []),
+    e.jsxs(e.Fragment, {
+      children: [
+        m &&
+          e.jsx("h1", {
+            className: "sessionExpired",
+            children: "session expired please login ",
+          }),
+        e.jsxs("form", {
+          className: "modern-form",
+          onSubmit: g,
+          children: [
+            e.jsx("div", { className: "form-title", children: "Sign In" }),
+            e.jsxs("div", {
+              className: "form-body",
+              children: [
+                e.jsx("div", {
+                  className: "input-group",
+                  children: e.jsxs("div", {
+                    className: "input-wrapper",
+                    children: [
+                      e.jsx("svg", {
+                        fill: "none",
+                        viewBox: "0 0 24 24",
+                        className: "input-icon",
+                        children: e.jsx("path", {
+                          strokeWidth: "1.5",
+                          stroke: "currentColor",
+                          d: "M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z",
+                        }),
+                      }),
+                      e.jsx("input", {
+                        required: !0,
+                        placeholder: "Username",
+                        className: "form-input",
+                        type: "text",
+                        value: s.username,
+                        onChange: (r) => n({ ...s, username: r.target.value }),
+                      }),
+                    ],
+                  }),
+                }),
+                e.jsxs("div", {
+                  className: "input-group",
+                  children: [
+                    e.jsxs("div", {
+                      className: "input-wrapper",
+                      children: [
+                        e.jsx("svg", {
+                          fill: "none",
+                          viewBox: "0 0 24 24",
+                          className: "input-icon",
+                          children: e.jsx("path", {
+                            strokeWidth: "1.5",
+                            stroke: "currentColor",
+                            d: "M12 10V14M8 6H16C17.1046 6 18 6.89543 18 8V16C18 17.1046 17.1046 18 16 18H8C6.89543 18 6 17.1046 6 16V8C6 6.89543 6.89543 6 8 6Z",
+                          }),
+                        }),
+                        e.jsx("input", {
+                          required: !0,
+                          placeholder: "Password",
+                          className: "form-input",
+                          type: "password",
+                          value: s.password,
+                          onChange: (r) =>
+                            n({ ...s, password: r.target.value }),
+                        }),
+                      ],
+                    }),
+                    i && e.jsx("p", { className: "error", children: i }),
+                  ],
+                }),
+              ],
+            }),
+            e.jsxs("button", {
+              className: "submit-button",
+              type: "submit",
+              disabled: p,
+              children: [
+                e.jsx("span", { className: "button-text", children: "Login" }),
+                e.jsx("div", { className: "button-glow" }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    })
+  );
+};
+export { w as default };
